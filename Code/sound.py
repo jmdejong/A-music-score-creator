d43bf09b706844810a8b9379c592157292be1672
Montagon <jose90clari@gmail.com>
2014-08-06 23:31:24 +0200

    A little amount of code.

---
+++ Code/sound.py
@@ -0,0 +1,52 @@
+import pyaudio
+import wave
+
+
+class filesound():
+	def __init__(self):
+		self.channels = 1
+		self.format = pyaudio.paInt16
+		self.rate = 44100
+
+
+def record():
+	CHUNK = 1024 
+	FORMAT = pyaudio.paInt16 #paInt8
+	CHANNELS = 1 
+	RATE = 44100 #sample rate
+	RECORD_SECONDS = 5
+	WAVE_OUTPUT_FILENAME = "output.wav"
+
+	p = pyaudio.PyAudio()
+
+	stream = p.open(format=FORMAT,
+	                channels=CHANNELS,
+	                rate=RATE,
+	                input=True,
+	                frames_per_buffer=CHUNK) #buffer
+
+	print("* recording")
+
+	frames = []
+
+	for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+	    data = stream.read(CHUNK)
+	    frames.append(data) # 2 bytes(16 bits) per channel
+
+	print("* done recording")
+
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+	return p
+	#wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+	#wf.setnchannels(CHANNELS)
+	#wf.setsampwidth(p.get_sample_size(FORMAT))
+	#wf.setframerate(RATE)
+	#wf.writeframes(b''.join(frames))
+	#wf.close()
+
+	return
+
+def save(name, file):
+	pass
